f177ed6bb406d74350d3741c56b1df78fb380bf0
Tom Hogue <tom.hogue.jr@gmail.com>
2019-10-24 12:02:56 -0700

    Cecys a big dum dum

--- src/getColorList.js
+++ src/getColorList.js
@@ -1,13 +1,20 @@
-const STATIC_COLORS = ["#8cb4d6", "#79b17d", "#ffd24b"];
+const STATIC_COLORS = [
+  '#2196f3',
+  '#90caf9',
+  '#6ec6ff',
+  '#0069c0',
+  '#c3fdff',
+  '5d99c6',
+]
 
 const getColorList = size => {
-  const colors = [...STATIC_COLORS];
+  const colors = [...STATIC_COLORS]
 
-  for (let idx = 3; idx < size; idx += 1) {
-    colors.push(`hsl(${Math.round(Math.random() * 360)}, 33%, 66%)`);
-  }
+  // for (let idx = 3; idx < size; idx += 1) {
+  //   colors.push(`hsl(${Math.round(Math.random() * 360)}, 33%, 66%)`);
+  // }
 
-  return colors.slice(0, size);
-};
+  return colors.slice(0, size)
+}
 
-export default getColorList;
+export default getColorList
